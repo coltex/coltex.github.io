0897e015ab78495dbce5001b40d04e4c5d4dd89a
Jonathan Darling <jmdarling.tx@gmail.com>
2014-01-17 17:01:19 -0600

    Update gitignore and MORE JS! (I should probably settle down on the JS a bit)

--- js/site.js
+++ js/site.js
@@ -1,7 +1,14 @@
-$(document).ready( function() {
-  $("#primary").hide(); 
-  $("#header").animo( { animation: 'bounceInLeft', duration: 1 }, function() {
-    $("#primary").show().animo( { animation: 'bounceInRight', duration: 1 } );
-  }); 
+$( function() {
+    $("#primary").hide();
+    $("#header").animo( { animation: 'bounceInLeft', duration: 1 }, function() {
+        $("#primary").show().animo( { animation: 'bounceInRight', duration: 1 });
+        $("#welcomeText").show().animo( { animation: 'pulse', duration: 2 });
+    });
+});
+
+$( function() {
+    $("#contactMeLink").click( function() {
+        $("#contactMeTab").click();
+    });
 });
 
