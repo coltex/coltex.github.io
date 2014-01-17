76bc8361ea7014ea3344a325498f5e4169c05f63
Jonathan Darling <jmdarling.tx@gmail.com>
2014-01-17 17:04:40 -0600

    Tones down the JS a bit.

--- js/site.js
+++ js/site.js
@@ -2,7 +2,6 @@ $( function() {
     $("#primary").hide();
     $("#header").animo( { animation: 'bounceInLeft', duration: 1 }, function() {
         $("#primary").show().animo( { animation: 'bounceInRight', duration: 1 });
-        $("#welcomeText").show().animo( { animation: 'pulse', duration: 2 });
     });
 });
 
