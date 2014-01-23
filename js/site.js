5f78ee5bd69748a111409eabd7bb13ae6b3c40ec
Jonathan Darling <jmdarling.tx@gmail.com>
2014-01-23 16:42:34 -0600

    Changed background to a dark one. Made header smaller. Added "scrollTo" functionality on the project accordian.

--- js/site.js
+++ js/site.js
@@ -1,3 +1,4 @@
+// Sexy opening animations.
 $( function() {
     $("#primary").hide();
     $("#header").animo( { animation: 'bounceInLeft', duration: 1 }, function() {
@@ -5,9 +6,18 @@ $( function() {
     });
 });
 
+// Makes the "contact me" link open the "contact me" tab.s
 $( function() {
     $("#contactMeLink").click( function() {
         $("#contactMeTab").click();
     });
 });
 
+// Scrolling for projects dropdowns.
+$( function () {
+    $(".project_link").click( function() {
+        $("html, body").animate({
+            scrollTop: $(this).parent().parent().offset().top
+        }, 1500);
+    });
+});
